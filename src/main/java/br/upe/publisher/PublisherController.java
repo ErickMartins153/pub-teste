b897a2bddb02710e4f4e5fcc07993eff22f0a09a
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-10-13 13:55:06 -0300

    adicionar lógica de temperatura

--- src/main/java/br/upe/publisher/PublisherController.java
+++ src/main/java/br/upe/publisher/PublisherController.java
@@ -5,22 +5,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class PublisherController {
     private RedisTemplate<String, Object> redisTemplate;
+    private ScheduledExecutorService executorService;
+    Scanner scanner = new Scanner(System.in);
 
     @Autowired
     public PublisherController(RedisTemplate<String, Object> redisTemplate) {
         this.redisTemplate = redisTemplate;
+        this.executorService = Executors.newScheduledThreadPool(1);
     }
 
     @PostConstruct
     public void publishMessage() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o canal que você deseja entrar:");
         String canal = scanner.nextLine();
+
+        if (Objects.equals(canal, "temperatura")) {
+            temperatura(canal);
+        } else {
+            chat(canal);
+        }
+    }
+
+    private void chat(String canal) {
         System.out.printf("Digite a mensagem para enviar para o canal '%s' (ou 'sair' para sair):", canal);
 
         while (true) {
@@ -34,6 +50,15 @@ public class PublisherController {
         }
         scanner.close();
 
+    }
+
+
+    private void temperatura(String canal) {
+        executorService.scheduleAtFixedRate(() -> {
+            String valor = new Random().nextFloat(1, 100) + " C";
+            redisTemplate.convertAndSend(canal, valor);
+            System.out.println("Temperatura enviada: " + valor);
+        }, 0, 5 + new Random().nextInt(6), TimeUnit.SECONDS);
     }
 }
 
